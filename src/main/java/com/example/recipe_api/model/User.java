a7fbc52c9ddc7b62765d6105948a33caf6486cea
Sasivy12 <muslimov.tim05@gmail.com>
2024-11-10 13:15:39 +0300

    added lombok and updated entities to use lombok annotations

--- src/main/java/com/example/recipe_api/model/User.java
+++ src/main/java/com/example/recipe_api/model/User.java
@@ -4,11 +4,16 @@ import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.*;
 
 import java.util.Set;
 
 @Entity
 @Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User
 {
     @Id
@@ -33,83 +38,4 @@ public class User
     @JsonManagedReference("user-recipes")
     private Set<Recipe> recipes;
 
-    public User(Long user_id, String username, String password, String email)
-    {
-        this.user_id = user_id;
-        this.username = username;
-        this.password = password;
-        this.email = email;
-    }
-
-    public User(String username, String password, String email)
-    {
-        this.username = username;
-        this.password = password;
-        this.email = email;
-    }
-
-    public User()
-    {
-
-    }
-
-    public Long getUser_id()
-    {
-        return user_id;
-    }
-
-    public void setUser_id(Long user_id)
-    {
-        this.user_id = user_id;
-    }
-
-    public String getUsername()
-    {
-        return username;
-    }
-
-    public void setUsername(String username)
-    {
-        this.username = username;
-    }
-
-    public String getPassword()
-    {
-        return password;
-    }
-
-    public void setPassword(String password)
-    {
-        this.password = password;
-    }
-
-    public String getEmail()
-    {
-        return email;
-    }
-
-    public void setEmail(String email)
-    {
-        this.email = email;
-    }
-
-    public Set<Review> getReviews()
-    {
-        return reviews;
-    }
-
-    public void setReviews(Set<Review> reviews)
-    {
-        this.reviews = reviews;
-    }
-
-    public Set<Recipe> getRecipes()
-    {
-        return recipes;
-    }
-
-    public void setRecipes(Set<Recipe> recipes)
-    {
-        this.recipes = recipes;
-    }
 }
